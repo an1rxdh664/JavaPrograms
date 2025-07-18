3ad987e1efc63613efad3b39bfb993e78bc7b687
an1rxdh664 <anirudhhh637@gmail.com>
2025-07-18 05:57:15 +0530

    "updates"

---
+++ Basics/Myprogram.java
@@ -0,0 +1,17 @@
+public class Myprogram{
+public static void main(String []arr){
+
+int a=5;
+int b=10;
+int c=15;
+
+int num;
+
+num=a+b+c;
+System.out.println("The addition of numbers is :" +num);
+
+num= c-a;
+System.out.println("The subraction of number is:" +num);
+}
+
+}
