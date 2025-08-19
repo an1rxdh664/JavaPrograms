6bc8c9d929691eb8b7482c3609c65ad3919ce66a
an1rxdh664 <anirudhhh637@gmail.com>
2025-08-19 16:28:18 +0530

    Rev string

--- Basics/functions.java
+++ Basics/functions.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
 public class functions {
-    public static int calArea(int length, int breadth){
-        return length * breadth;
+    // public static int calArea(int length, int breadth){
+    //     return length * breadth;
+    // }
+
+    public static String revString(String inputString){
+        String rev = "";
+        for(int i=inputString.length()-1;i>=0;i--){
+            rev += inputString.charAt(i);
+        }
+        return rev;
     }
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        System.out.print("Enter length : ");
-        int l = inp.nextInt();
-        System.out.print("Enter breadth : ");
-        int b = inp.nextInt();
-        System.out.print("Area : " + calArea(l, b));
+        // System.out.print("Enter length : ");
+        // int l = inp.nextInt();
+        // System.out.print("Enter breadth : ");
+        // int b = inp.nextInt();
+        // System.out.print("Area : " + calArea(l, b));
+        
+        
+        System.out.print("Enter String : ");
+        String inString = inp.next();
+        System.out.println("Reversed String : " + revString(inString));
     }
 }
