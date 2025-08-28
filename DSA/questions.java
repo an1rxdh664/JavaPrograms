1e468987339a4df9e5056b6cb365ab804eb07b41
an1rxdh664 <anirudhhh637@gmail.com>
2025-08-28 11:49:26 +0530

    House robber

--- DSA/questions.java
+++ DSA/questions.java
@@ -1,5 +1,23 @@
+import java.util.*;
 public class questions{
+
+
+    // leetcode 198
+    public static int houseRobber(){
+        int[] nums = {1,2,3,1};
+        int prev1 = 0;
+        int prev2 = 0;
+        int current = 0;
+        for(int i=0;i<nums.length;i++){
+            current = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("we are back");
+        
     }
 }
