05c29ef44360b668ea121133778bbd65b5900372
an1rxdh664 <anirudhhh637@gmail.com>
2025-08-29 13:57:33 +0530

    Multi dimensional ArrayLists

--- DSA/arrayList.java
+++ DSA/arrayList.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 public class arrayList {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<Integer>(5); // this is how an array list is created
         // initial size of this array list is 5
 
@@ -12,12 +14,33 @@ public class arrayList {
         // System.out.print(list);
 
         // add / remove methods
-        list.add(5);
-        list.add(3);
-        list.add(4);
-        list.add(1);
-        System.out.println(list);
-        list.remove(4);
-        System.out.println(list);
+        // list.add(5);
+        // list.add(3);
+        // list.add(4);
+        // list.add(1);
+        // System.out.println(list);
+
+        // list.remove(4);
+        // System.out.println(list);
+
+        // Multidimensional arrays in array list
+        ArrayList<ArrayList<Integer>> exList = new ArrayList<>();
+        // array list of array lists
+
+        // initialisation for each element of the current array list
+        for (int i = 0; i < 3; i++) {
+            exList.add(new ArrayList<>());
+        }
+
+
+        // storing values --
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                exList.get(i).add(in.nextInt());
+            }
+        }
+
+        System.out.println(exList); // [[1, 2, 3], [5, 4, 9], [7, 8, 5]]
+
     }
 }
